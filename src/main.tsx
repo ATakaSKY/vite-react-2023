a77b71c5b41d302754a572fae5326d531c1c8d94
Aakash Thakur <thakurakash321@gmail.com>
2023-07-25 13:21:56 +0530

    feat: redux setup
    added redux toolkit for global state management

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import Router from 'Router';
+import store from 'store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
    <React.StrictMode>
-      <RouterProvider router={Router} />
-   </React.StrictMode>,
+      <Provider store={store}>
+         <RouterProvider router={Router} />
+      </Provider>
+   </React.StrictMode>
 );
